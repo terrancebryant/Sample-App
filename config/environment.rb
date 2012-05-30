dbfb96aa791e4dbecc54b7ac6591551aa3dceb36
Terrance Bryant <tbryant@teliax.com>
2012-05-30 11:58:20 -0600

    Initial Commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+PortfolioV1::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortfolioV1::Application.config.secret_token = '3d9d9f1689f8c13975260dc8f2dd22b62e889e96ad330c3aaec191ee2677507681e29da587574b1f7af86aefb3994a3b65c865e4f231d2b25820c2eb90b562bb'
